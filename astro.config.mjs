352ca0063786847757e4901bf2d98b9992b779cb
lucagazze <lucagazze1@gmail.com>
2026-02-26 10:17:47 -0300

    fix: change astro build output to server to allow API routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://algoritmiadesarrollos.com.ar',
-  output: "static",
+  output: "server",
   adapter: vercel({
     webAnalytics: {
       enabled: true,
